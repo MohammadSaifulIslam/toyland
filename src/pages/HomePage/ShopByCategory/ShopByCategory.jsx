126a8745bf822c5b91331a26c21489979c50deed
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-05-17 23:58:26 -0700

    subCategory style changed and made it raw without react tab

--- src/pages/HomePage/ShopByCategory/ShopByCategory.jsx
+++ src/pages/HomePage/ShopByCategory/ShopByCategory.jsx
@@ -1,54 +1,38 @@
 import { useEffect, useState } from "react";
 import ToyCard from "./ToyCard/ToyCard";
 
-// react tab
-import { Tab, TabList, TabPanel, Tabs } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
 
 
 const ShopByCategory = () => {
-    const [currentTab, setCurrentTab] =useState('Stuffed Animals');
+    const [currentTab, setCurrentTab] = useState('Stuffed Animals');
     const [toys, setToys] = useState([])
+
+    const handleCurrentTab = (e) => {
+        setCurrentTab(e.target.innerHTML);
+    }
     console.log(currentTab)
 
-    
     useEffect(() => {
         fetch(`http://localhost:5000/toys-by-subCategory/${currentTab}`)
             .then(res => res.json())
             .then(data => setToys(data))
     }, [currentTab])
 
-    
-    console.log(toys)
+    const subCategory = ['Stuffed Animals', 'Wildlife', 'Sea Creatures', 'Woodland Animals', 'Marsupials', 'Birds'];
     return (
-        <section className="my-container my-20">
-            <Tabs>
-                <TabList>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Stuffed Animals</span></Tab>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Wildlife</span></Tab>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Sea Creatures</span></Tab>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Woodland Animals</span></Tab>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Marsupials</span></Tab>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Birds</span></Tab>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Interactive Toys</span></Tab>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Farm Animals</span></Tab>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Plush Toys</span></Tab>
-                    <Tab><span onClick={(e)=> setCurrentTab(e.target.innerHTML)}>Action Figures</span></Tab>
-                </TabList>
-
-                <TabPanel> </TabPanel>
-                <TabPanel> </TabPanel>
-                <TabPanel> </TabPanel>
-                <TabPanel> </TabPanel>
-                <TabPanel> </TabPanel>
-                <TabPanel> </TabPanel>
-                <TabPanel> </TabPanel>
-                <TabPanel> </TabPanel>
-                <TabPanel> </TabPanel>
-                <TabPanel> </TabPanel>
-               
-            </Tabs>
-            <div className="grid md:grid-cols-3 gap-5">
+        <section className="my-container py-20">
+            <div className="section-title mb-10 text-center lg:w-2/3 mx-auto">
+                <h3 className="text-3xl font-bold mb-5">Explore a World of Playful Possibilities</h3>
+                <p className="font-medium">Discover our diverse range of toys categorized to suit every childs interests and preferences. From action-packed superheroes to cuddly companions, our Toys by Category section ensures endless hours of fun and entertainment for children of all ages.</p>
+            </div>
+            <div className="shadow-lg bg-white mb-10 md:flex justify-between items-center">
+                {
+                    subCategory.map((sub, index) => <p
+                        key={index}
+                        className={`${sub == currentTab ? 'text-white bg-[#12aee0]' : ''} tab-outline`} onClick={handleCurrentTab}>{sub}</p>)
+                }
+            </div>
+            <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-5">
                 {
                     toys.map(toy => <ToyCard
                         key={toy._id}
--- src/pages/HomePage/ShopByCategory/ToyCard/ToyCard.jsx
+++ src/pages/HomePage/ShopByCategory/ToyCard/ToyCard.jsx
@@ -3,13 +3,13 @@ const ToyCard = ({ toy }) => {
     const { pictureURL, name, price, rating } = toy;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
-            <figure><img src={pictureURL} alt="Shoes" /></figure>
+            <figure><img src={pictureURL} alt="Shoes" className="w-full h-80 md:h-96 object-contain" /></figure>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>Price: {price}</p>
+                <h2 className="text-2xl font-bold">{name}</h2>
+                <p className="text-lg font-semibold">Price: <span className="text-[#12aee0]">${price}</span></p>
                 <p>Rating: {rating}</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <div className="card-actions">
+                    <button className="my-btn">View Details</button>
                 </div>
             </div>
         </div>
