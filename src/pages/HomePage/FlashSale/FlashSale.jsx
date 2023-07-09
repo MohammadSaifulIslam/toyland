c5a0fdfe6a9e8f95a0de8295fa31c5a8c137fd20
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-07-09 00:24:57 -0700

    flash sale section added

--- src/pages/HomePage/FlashSale/FlashSale.jsx
+++ src/pages/HomePage/FlashSale/FlashSale.jsx
@@ -1,16 +1,66 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/pagination';
+
+
+// import required modules
+import { useEffect, useState } from 'react';
+import { FreeMode } from 'swiper';
 
 const FlashSale = () => {
-
-
+    const [toys, setToys] = useState([])
+    useEffect(() => {
+        fetch('https://toyland-server.vercel.app/all-toys')
+            .then(res => res.json())
+            .then(data => setToys(data))
+    }, [])
+    console.log({ toys })
     return (
-        <section className='pt-20 pb-32 bg-[#e5f9ff] bottom-curve overflow-hidden'>
+        <section className='py-20'>
             <div className="my-container">
                 <div className="section-title mb-10 text-center lg:w-2/3 mx-auto">
                     <h3 className="text-3xl font-bold mb-5">Flash Sale</h3>
                     <p className="font-medium">Enchanting Moments Captured in Play. Immerse yourself in a delightful gallery showcasing the magic of playtime.</p>
                 </div>
                 <div>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia libero assumenda eius iusto corrupti eum atque illo voluptates, exercitationem quia impedit obcaecati nihil animi at numquam ut delectus! Officia, beatae.</p>
+                    <Swiper
+                        breakpoints={{
+                            576: {
+                                // width: 576,
+                                slidesPerView: 2,
+                            },
+                            768: {
+                                // width: 768,
+                                slidesPerView: 3,
+                            },
+                            992: {
+                                // width: 768,
+                                slidesPerView: 4,
+                            }
+                        }}
+                        spaceBetween={30}
+                        freeMode={true}
+
+                        modules={[FreeMode,]}
+                        className="mySwiper"
+                    >
+                        {
+                            toys.slice(5, 10).map(toy => <SwiperSlide key={toy._id}>
+                                <div>
+                                    <figure className='relative'>
+                                        <img src={toy.pictureURL} alt="toy image" />
+                                        <div className='absolute py-2 px-5  top-0 right-0 bg-[#12aee0]'><p className='text-sm font-medium text-white'>${toy.price} 30% Off</p></div>
+                                    </figure>
+                                        <h2 className='font-semibold text-xl '>{toy.name}</h2>
+                                      
+                                </div>
+                            </SwiperSlide>)
+                        }
+                    </Swiper>
                 </div>
             </div>
         </section>
--- src/pages/MyToyPage/MyToyTableRow/MyToyTableRow.jsx
+++ src/pages/MyToyPage/MyToyTableRow/MyToyTableRow.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 const MyToyTableRow = ({ toy, index, handleDeleteToy, setToyId }) => {
     const { _id, name, pictureURL, availableQuantity, price } = toy;
 
-
     return (
         <>
             <tr>
@@ -20,27 +19,26 @@ const MyToyTableRow = ({ toy, index, handleDeleteToy, setToyId }) => {
                 <td>
                     {name}
                 </td>
-<<<<<<< HEAD
                 <td>
                     {availableQuantity}
                 </td>
                 <td>{price}</td>
-=======
                 <td>{price}</td>
                 <td>
                     {availableQuantity}
                 </td>
->>>>>>> 92d7c4a4855769df97f278881c9adc3b83de17bd
+
+
                 <td>
-                 <Link to={`/toy/${_id}`}>
-                 <FaEye className='text-xl cursor-pointer text-[#12aee0]'></FaEye>
-                 </Link>
+                    <Link to={`/toy/${_id}`}>
+                        <FaEye className='text-xl cursor-pointer text-[#12aee0]'></FaEye>
+                    </Link>
                 </td>
                 <td>
-                    <label htmlFor="my-modal-3" onClick={()=> setToyId(_id)}>
-                    <FaEdit className='text-xl cursor-pointer text-[#12aee0]' />
+                    <label htmlFor="my-modal-3" onClick={() => setToyId(_id)}>
+                        <FaEdit className='text-xl cursor-pointer text-[#12aee0]' />
                     </label>
-                   
+
                 </td>
                 <td>
                     <FaTrashAlt onClick={() => handleDeleteToy(_id)} className='text-xl cursor-pointer text-red-500' />
