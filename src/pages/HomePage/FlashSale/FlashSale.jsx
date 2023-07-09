2167a88108d0b92271f43ec3c3373d7a98cf3add
Mohammad Saiful Islam <saifmdislam231@gmail.com>
2023-07-08 23:51:35 -0700

    test change

--- src/pages/HomePage/FlashSale/FlashSale.jsx
+++ src/pages/HomePage/FlashSale/FlashSale.jsx
@@ -1,11 +1,16 @@
 
 const FlashSale = () => {
+
+
     return (
         <section className='pt-20 pb-32 bg-[#e5f9ff] bottom-curve overflow-hidden'>
             <div className="my-container">
                 <div className="section-title mb-10 text-center lg:w-2/3 mx-auto">
                     <h3 className="text-3xl font-bold mb-5">Flash Sale</h3>
                     <p className="font-medium">Enchanting Moments Captured in Play. Immerse yourself in a delightful gallery showcasing the magic of playtime.</p>
+                </div>
+                <div>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Mollitia libero assumenda eius iusto corrupti eum atque illo voluptates, exercitationem quia impedit obcaecati nihil animi at numquam ut delectus! Officia, beatae.</p>
                 </div>
             </div>
         </section>
